61d310be88127117ea3881276a617f5cc500505d
xnacly <47723417+NaCly@users.noreply.github.com>
2024-10-04 21:03:17 +0200

    lexer: support for comments and nested comments

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -28,6 +28,20 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn next_equals(&mut self, c: char) -> bool {
+        match self.source.get(self.pos + 1) {
+            Some(cc) => *cc == c as u8,
+            _ => false,
+        }
+    }
+
+    fn equals(self, c: char) -> bool {
+        match self.source.get(self.pos) {
+            Some(cc) => *cc as char == c,
+            _ => false,
+        }
+    }
+
     pub fn run(&mut self) -> Vec<Token> {
         let mut r = vec![];
         if self.source.len() == 0 {
@@ -38,6 +52,50 @@ impl<'a> Lexer<'a> {
             ));
             return vec![];
         };
+
+        loop {
+            let cc = match self.source.get(self.pos) {
+                Some(cc) => *cc,
+                _ => break,
+            } as char;
+            match cc {
+                '/' => {
+                    if self.next_equals('*') {
+                        loop {
+                            self.pos += 1;
+                            if let Some(cc) = self.source.get(self.pos) {
+                                if *cc as char == '*' {
+                                    if let Some(cc) = self.source.get(self.pos + 1) {
+                                        if *cc as char == '/' {
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                '-' => {
+                    if self.next_equals('-') {
+                        loop {
+                            self.pos += 1;
+                            if let Some(cc) = self.source.get(self.pos) {
+                                if *cc as char == '\n' {
+                                    break;
+                                }
+                            };
+                        }
+                    }
+                }
+                _ => {
+                    // TODO: keywords and integers
+                    // let start = self.pos;
+                    // while self.equals() {}
+                }
+            }
+            self.pos += 1;
+        }
+
         if r.len() == 0 {
             self.errors.push(self.err(
                 "No statements found in source file",
--- tests/comment.sql
+++ tests/comment.sql
@@ -2,3 +2,7 @@
 -- comments, but empty input
 
 -- comments, but empty input
+
+/* 
+comments are weird man 
+*/
