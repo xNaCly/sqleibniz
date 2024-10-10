8db95419edcac44c850b3a9120e56b1bef6beef7
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-10-10 16:05:51 +0200

    rules: use serde to deserialize the configuration

--- src/rules.rs
+++ src/rules.rs
@@ -1,28 +1,29 @@
-#[derive(Debug)]
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, PartialEq)]
 pub enum Rule {
     /// NoContent is emitted when the source file is empty
     NoContent,
     /// NoStatements is emitted when the source file is not empty but holds no statements
     NoStatements,
-    /// Unimplemented is emitted when the source file contains constructs sqleibniz does not yet
-    /// understand
+    /// Unimplemented is emitted when the source file contains constructs sqleibniz does not yet understand
     Unimplemented,
     /// Unterminated is emitted when the source file contains unterminated strings
     UnterminatedString,
 }
 
-// TODO: serialize this via serde, convert strings to enum fields, implement the following of these
-// rules
+#[derive(Deserialize, Debug)]
+/// Allows for disabling diagnostics and turning off behaviours
+pub struct Disabled {
+    pub rules: Vec<Rule>,
+}
 
+#[derive(Deserialize, Debug)]
 pub struct Config {
-    pub disabled: Vec<String>,
+    pub disabled: Disabled,
 }
 
 impl Rule {
-    pub fn from(s: &str) -> Option<Rule> {
-        return None;
-    }
-
     pub fn to_str(&self) -> &str {
         match self {
             Self::NoContent => "NoContent",
