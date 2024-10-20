d3de279875193288519feb4c0bf1012b4c24a232
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-10-20 21:44:32 +0200

    parser: node generation via macro

--- src/parser/nodes.rs
+++ src/parser/nodes.rs
@@ -1,12 +1,25 @@
 use crate::types::Token;
 
-pub trait Node: std::fmt::Debug {}
-
-// TODO: write a macro to automate this
-// node!(Literal, t: Token) -> below
-
+/// Generates a Node from the given input:
+///
+///     node!(Literal, t: Token);
+///     // generates:
+///     #[derive(Debug)]
+///     pub struct Literal {
+///         pub t: Token,
+///     }
+///     impl Node for Literal {}
+macro_rules! node {
+    ($node_name:ident,$($field_name:ident:$field_type:ty),*) => {
 #[derive(Debug)]
-pub struct Literal {
-    pub t: Token,
+pub struct $node_name {
+    $(
+        pub $field_name: $field_type,
+    )*
 }
-impl Node for Literal {}
+impl Node for $node_name {}
+    };
+}
+
+pub trait Node: std::fmt::Debug {}
+node!(Literal, t: Token);
