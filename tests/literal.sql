a8fdf8febc95db0e0a5f0e2e84a3c1c09d7f18b8
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-10-21 10:09:52 +0200

    tests/literal: reordered literals to match flow chart

--- tests/literal.sql
+++ tests/literal.sql
@@ -1,25 +1,25 @@
 -- literals: https://www.sqlite.org/syntax/literal-value.html
 
--- numbers see https://www.sqlite.org/syntax/numeric-literal.html
-1_000.12_000e+3_5;
-.1_000e-1_2;
-0xabcdef1;
-0XABCDEF1;
-
 -- strings:
 'string1';
 'string202192ijkandkajdohnakd';
 'string3';
 'string202192ijkandkajdohnakdstring202192ijkandkajdohnakdstring202192ijkandkajdohnakdstring202192ijkandkajdohnakdstring202192ijkandkajdohnakdstring202192ijkandkajdohnakd';
 
+-- numbers: see https://www.sqlite.org/syntax/numeric-literal.html
+1_000_120e+35;
+.1_000e-12;
+0xabcdef1;
+0XABCDEF1;
+
 -- blob:
 x'abcdef01234567890';
 X'ABCDEF01234567890';
 
--- null (technically a keyword)
+-- null (technically a keyword):
 NULL;
 
--- booleans
+-- booleans:
 true;
 false;
 
